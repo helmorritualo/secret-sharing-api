a3d5968a42e803e030d713f025996f48e2b58292
Helmor Ritualo <helmorritualo@gmail.com>
2025-07-07 17:20:40 +0800

    fix: change the timestamp to schemas

--- src/models/auditlog.ts
+++ src/models/auditlog.ts
@@ -1,12 +1,16 @@
 import { Schema, model } from "mongoose";
 
-const AuditLogSchema = new Schema({
-  secretId: { type: String, required: true },
-  event: { type: String, required: true }, // "VIEWED", "EXPIRED", "INVALID_OTP"
-  ip: { type: String },
-  userAgent: { type: String },
-  timestamp: { type: Date, default: Date.now },
-});
+const AuditLogSchema = new Schema(
+  {
+    secretId: { type: String, required: true },
+    event: { type: String, required: true }, // "VIEWED", "EXPIRED", "INVALID_OTP"
+    ip: { type: String },
+    userAgent: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const AuditLog = model("AuditLog", AuditLogSchema);
 
--- src/models/secrets.ts
+++ src/models/secrets.ts
@@ -1,13 +1,17 @@
 import { Schema, model } from "mongoose";
 
-const SecretSchema = new Schema({
-  _id: { type: String, required: true },
-  content: { type: String, required: true },
-  otp: { type: String }, // Optional hashed OTP
-  expiresAt: { type: Date, required: true },
-  viewed: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
-});
+const SecretSchema = new Schema(
+  {
+    _id: { type: String, required: true },
+    content: { type: String, required: true },
+    otp: { type: String }, // Optional hashed OTP
+    expiresAt: { type: Date, required: true },
+    viewed: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Secret = model("Secret", SecretSchema);
 
